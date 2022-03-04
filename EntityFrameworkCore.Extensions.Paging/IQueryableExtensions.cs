9d5f60f54a00042af06b3463a6f341b7c9448e17
Petr Ervin Palas <petr.palas@lundegaard.eu>
2022-03-04 21:08:36 +0100

    Readme update and param name to be the same

--- EntityFrameworkCore.Extensions.Paging/IQueryableExtensions.cs
+++ EntityFrameworkCore.Extensions.Paging/IQueryableExtensions.cs
@@ -5,12 +5,20 @@ namespace EntityFrameworkCore.Extensions.Paging
 {
     public static class IQueryableExtensions
     {
+        /// <summary>
+        /// Gets a PagedResult<T> from IQueryable<T>. Has to used last similar to Single() or ToList()
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="query"></param>
+        /// <param name="pageIndex"></param>
+        /// <param name="pageSize"></param>
+        /// <returns></returns>
         public static PagedResult<T> GetPaged<T>(this IQueryable<T> query,
-            int page, int pageSize) where T : class
+            int pageIndex, int pageSize = 50) where T : class
         {
             var result = new PagedResult<T>
             {
-                CurrentPage = page,
+                CurrentPage = pageIndex,
                 PageSize = pageSize,
                 RowCount = query.Count()
             };
@@ -19,7 +27,7 @@ namespace EntityFrameworkCore.Extensions.Paging
             var pageCount = (double)result.RowCount / pageSize;
             result.PageCount = (int)Math.Ceiling(pageCount);
 
-            var skip = (page - 1) * pageSize;
+            var skip = (pageIndex - 1) * pageSize;
             result.Results = query.Skip(skip).Take(pageSize).ToList();
 
             return result;
--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Provides IQueriable extension to grab specific pagedResult from query utilizing Skip and Take LINQ internaly.
 
+Entry parameters are ```pageIndex``` which is a page index 1-N and ```pageSize``` to define how many max items are on a page, default is 50.
+
 ```cs
 var pagedResult = DbContext.EntitySer.Where(/* where condition */).GetPaged(pageIndex, pageSize);
 ```
